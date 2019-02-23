651d63aff25e30b6bc67dcac23e5fcc7ed382835
Alex <Alexiosdut@gmail.com>
2019-02-24 00:41:17 +0200

    implement fake routing

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Home from "./Home";
 import Results from "./Results";
 
 class App extends Component {
+  state = {
+    location: "home"
+  };
+
+  routingSystem = () => {
+    switch (this.state.location) {
+      case "home":
+        return <Home />;
+        break;
+      case "results":
+        return <Results />;
+        break;
+      default:
+        return <Home />;
+    }
+  };
+
   render() {
     return (
       <div className="container">
@@ -15,7 +32,7 @@ class App extends Component {
             </a>
           </nav>
         </header>
-        <Results />
+        {this.routingSystem()}
       </div>
     );
   }
